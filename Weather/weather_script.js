2980c6cb3fab32eec6a0777968e63bb103f3e53d
Radha1253 <rajninitturi@gmail.com>
2025-07-28 15:44:43 -0500

    did the CSS and styling of the weather page

---
+++ Weather/weather_script.js
@@ -0,0 +1,4 @@
+<script>
+    const apiKey = "c7230e9f40aa220f9c57a6c5d53ff1ea";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=houston&appid=c7230e9f40aa220f9c57a6c5d53ff1ea&units=metric";
+</script>
